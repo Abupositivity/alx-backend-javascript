3cce9212042967bc54bd711927bc73c77567bc2c
abupositivity <abupositivity@gmail.com>
2024-09-22 03:05:28 +0000

    complex HTTP server using express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,44 +2,42 @@ const express = require('express');
 const fs = require('fs');
 
 // Function to read students asynchronously from a CSV file
-const countStudents = (databasePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(databasePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const countStudents = (databasePath) => new Promise((resolve, reject) => {
+  fs.readFile(databasePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const studentRecords = lines.slice(1); // Remove the header
+    const studentsByField = {};
+
+    studentRecords.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
       }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-      if (lines.length <= 1) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const studentRecords = lines.slice(1); // Remove the header
-      const studentsByField = {};
-
-      studentRecords.forEach((line) => {
-        const [firstname, , , field] = line.split(',');
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-        }
-        studentsByField[field].push(firstname);
-      });
-
-      const totalStudents = studentRecords.length;
-      let result = `Number of students: ${totalStudents}\n`;
-
-      Object.keys(studentsByField).forEach((field) => {
-        const students = studentsByField[field];
-        result += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
-      });
-
-      resolve(result.trim());
+      studentsByField[field].push(firstname);
     });
+
+    const totalStudents = studentRecords.length;
+    let result = `Number of students: ${totalStudents}\n`;
+
+    Object.keys(studentsByField).forEach((field) => {
+      const students = studentsByField[field];
+      result += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+    });
+
+    resolve(result.trim());
   });
-};
+});
 
 // Create an instance of an Express application
 const app = express();
