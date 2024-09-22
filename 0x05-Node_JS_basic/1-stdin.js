5c01a97cb1fe182389fcb0f15a399bc0e9ca7dc0
abupositivity <abupositivity@gmail.com>
2024-09-22 02:09:25 +0000

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,13 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input from stdin
 process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  const name = process.stdin.read();
 
-  // Close the program gracefully
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
